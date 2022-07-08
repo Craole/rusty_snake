ae7cfb40aca33fd23f6246d1c4d328c0457dc9d7
craole <iamcraole@gmail.com>
2022-07-08 07:03:43 -0500

    initial build

--- readme.md
+++ readme.md
@@ -2,6 +2,33 @@
 
 Classic snake game implemented using Rust & WASM
 
+## Prerequisites
+
+- rust
+- wasm-pack
+
+  Linux
+
+  ```curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+  ```
+
+  Windows 64-bit
+  <https://github.com/rustwasm/wasm-pack/releases/download/v0.10.3/wasm-pack-init.exe>
+
+  Other Systems
+
+  ```sh
+  cargo install wasm-pack
+  ```
+
+  Using Node
+
+  ```sh
+  npm install -g wasm-pack
+  yarn global add wasm-pack
+  pnpm install -g wasm-pack
+  ```
+
 ## References
 
 [Let's Code Snake with Rust and WASM](https://www.youtube.com/watch?v=iR7Q_6quwSI&list=PLtTT8p-gjGEdGzZ0ET2bwNnA6iP_mmmrv)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+mod random;
+use std::{collections::VecDeque, vec};
+
+use random::random_range;
+
 pub type Position = (usize, usize);
 
 #[derive(Debug)]
@@ -13,9 +18,10 @@ pub enum Direction {
 pub struct SnakeGame {
     width: usize,
     height: usize,
-    snake: Vec<Position>, // Head is the first item and Tail is the last
+    snake: VecDeque<Position>, // Head is the first item and Tail is the last
     direction: Direction,
     food: Position,
+    endgame: bool,
 }
 
 impl SnakeGame {
@@ -23,10 +29,67 @@ impl SnakeGame {
         Self {
             width,
             height,
-            snake: vec![((width - 2).max(0), height / 2)],
+            snake: vec![((width - 2).max(0), height / 2)].into_iter().collect(),
             direction: Direction::Left,
             food: (2.min(width - 1), height / 2),
+            endgame: false,
         }
+    }
+
+    pub fn change_direction(&mut self, direction: Direction) {
+        match (&self.direction, direction) {
+            (Direction::Up, Direction::Up)
+            | (Direction::Up, Direction::Down)
+            | (Direction::Right, Direction::Right)
+            | (Direction::Right, Direction::Left)
+            | (Direction::Down, Direction::Up)
+            | (Direction::Down, Direction::Down)
+            | (Direction::Left, Direction::Right)
+            | (Direction::Left, Direction::Left) => {}
+            (_, direction) => self.direction = direction,
+        }
+    }
+    pub fn is_valid(&mut self, (x, y): Position) -> bool {
+        x < self.width && y < self.height
+    }
+    pub fn tick(&mut self) {
+        // endgame
+        if self.endgame && self.snake.len() == 0 {
+            return;
+        }
+        // Move
+
+        let (x, y) = self.snake[0];
+        let new_head = match &self.direction {
+            Direction::Up => (x, y - 1),
+            Direction::Right => (x + 1, y),
+            Direction::Down => (x, y + 1),
+            Direction::Left => (x - 1, y),
+        };
+
+        if !self.is_valid(new_head) || self.snake.contains(&new_head) {
+            self.endgame = true;
+        } else {
+            if new_head != self.food {
+                self.snake.pop_back();
+            } else {
+                //| Identify a valid food position
+                let free_positions = (0..self.height)
+                    .flat_map(|y| (0..self.width).map(move |x| (x, y)))
+                    .filter(|pos| !self.snake.contains(pos))
+                    .collect::<Vec<_>>();
+
+                //| Ensure that the food is not a part of the existing snake
+                if free_positions.is_empty() {
+                    self.endgame = true;
+                    return;
+                }
+
+                //| Randomly generate a new food position
+                self.food = free_positions[random_range(0, free_positions.len())];
+            }
+        }
+        self.snake.push_front(new_head);
     }
 }
 
